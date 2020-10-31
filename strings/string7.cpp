e484ac79611ab38e529c41aab957c6cce77993b9
nipun arora <aroranipun1@gmail.com>
2020-10-31 17:05:50 +0530

    added some string programs

---
+++ strings/string7.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s = "1";
+        
+        while(--n){
+            string ns;//new string
+            int i=0;
+                while(i < s.size()){
+                    int count = 1;
+                        while(i+1 < s.size() && s[i] == s[i+1]){
+                            count++;
+                            i++;
+                        }
+                    ns += to_string(count) + s[i];
+                    i++;
+                }
+            s = ns;
+         }
+        return s;
+    }
+};
