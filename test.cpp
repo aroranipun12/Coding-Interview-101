eba2de6868abf7b904e8dc7c6c301f67a4dad633
nipun arora <aroranipun1@gmail.com>
2020-11-02 20:39:54 +0530

    pushing more programs

--- test.cpp
+++ test.cpp
@@ -1,43 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-//this is when k = 1(difference b/w adj elements is 1)
-int index_of(int* arr,int n,int target){
-	int i=0;
-	while(i<n){
-		if(arr[i] == target)
-			return i;
-		else
-			i += max(1,abs(arr[i] - target)); 
-	}
-return -1;//not found
+int c=0;
+int fs(string s1,string s2,int ps1,int ps2,vector<vector<int>> &data){int i=ps2;
+  
+    if(data[ps1][ps2]==-1){while(i<s2.length()&&s2[i]!=s1[ps1])i++;
+                          
+                        if(i==s2.length())data[ps1][ps2]=1;
+                           else{
+                               data[ps1][ps2]=min(fs(s1,s2,ps1+1,ps2,data),1+fs(s1,s2,ps1+1,i+1,data));}
+                           
+                          
+                          
+        
+    }
+    c++;
+    return(data[ps1][ps2]);
 }
-
-//we can move to diff/k
-int index_of_k(int* arr,int n,int k,int target){
-int i=0;
-	while(i<n){
-		if(arr[i] == target)
-			return i;
-		else
-			i += max(1,abs(arr[i] - target)/k);//diff/k 
-	}
-return -1;//not found
-}
-
-int main( int argc , char ** argv )
+int solve(string S,string V)
 {
-	ios_base::sync_with_stdio(false) ; 
-	cin.tie(NULL) ; 
+	// Write your code here.
+    int n=S.length(),m=V.length();
+    vector<vector<int>> data(n+1,vector<int>(m+1));
+    for(int i=0;i<n;i++){data[i].assign(m+1,-1);data[i][m]=1;}data[n].assign(m+1,100000);
+    return(fs(S,V,0,0,data));
+}
 
 
-	int n,x,k;
-	cin>>n>>x>>k;
-	int* arr = new int[n];
-	for(int i=0;i<n;i++)
-		cin>>arr[i];
-	cout<<endl;
-	cout<<index_of_k(arr,n,k,x);
-	
-	return 0;
+int main()
+{
+    string S,V;
+    cin>>S>>V;
+    cout<<solve(S,V)<<endl;
+    cout<<c;
+
+    return 0;
 }
